5c646c668c4d0928246d93dcb3ed7edb14d67b17
Rafael William <rafael.fim@hotmail.com>
2023-05-12 00:30:19 -0300

    handle env with zod

--- src/database.ts
+++ src/database.ts
@@ -1,16 +1,12 @@
-import 'dotenv/config' 
 import { knex as setupKnex, Knex } from 'knex'
-
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL env not found')
-}
+import { env } from './env'
 
 export const config: Knex.Config = {
     // Informações obrigatórios
     client: 'sqlite',
     // para conexões com outros bancos, ver a documentação
     connection: {
-        filename: process.env.DATABASE_URL,
+        filename: env.DATABASE_URL
     },
     useNullAsDefault: true,
     migrations: {
