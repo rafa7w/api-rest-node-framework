cfd0d2507503241e4554d05202271015551b2c05
Rafael William <rafael.fim@hotmail.com>
2023-05-12 09:46:30 -0300

    plugins fastify

---
+++ src/routes/transactions.ts
@@ -0,0 +1,28 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { randomUUID } from 'node:crypto'
+import { knex } from '../database'
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit'])
+    })
+
+    // validando os dados do request.body para ver se batem com o schema definido
+    const { title, amount, type } = createTransactionBodySchema.parse(request.body)
+    // nada aqui embaixo vai executar se não passar o parse, pois dá um throw no erro
+
+    await knex('transactions').insert({
+      id: randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1
+    })
+
+    // em rotas criação dentro da nossa API, geralmente não se faz retornos
+    return reply.status(201).send()
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,12 @@
 import fastify from 'fastify'
 // import crypto from 'node:crypto'
-import { knex } from './database'
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
 
-app.get('/hello', async () => {
-    /* const transaction = await knex('transaction').insert({
-        id: crypto.randomUUID(),
-        title: 'Transação de teste',
-        amount: 1000,
-    }).returning('*') */
-
-    // const transactions = await knex('transaction').select('*')
-
-    const transaction = await knex('transaction').where('amount', 1000).select('*')
-
-
-    return transaction
+app.register(transactionsRoutes, {
+    prefix: 'transactions'
 })
 
 app.listen({
