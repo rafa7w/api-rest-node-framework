ca853e2618bb5fab7903aa36a2cdcb8437d6fbc2
Rafael William <rafael.fim@hotmail.com>
2023-05-12 14:36:18 -0300

    testing list of transactions

--- src/test/transactions.spec.ts
+++ src/test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { it, test, beforeAll, afterAll, describe, expect } from 'vitest'
 import request from 'supertest' 
 import { app } from '../app'
 
@@ -32,5 +32,29 @@ describe('Transactions routes', () => {
         amount: 5000,
         type: 'credit'
       }).expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'New transaction',
+        amount: 5000,
+        type: 'credit'
+      }).expect(201)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+    
+    const listTransactionResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies) // o set é usado para configurar um cabeçalho da requisição
+      .expect(200)
+
+    expect(listTransactionResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'New transaction',
+        amount: 5000,
+      })
+    ])
   })
 })
