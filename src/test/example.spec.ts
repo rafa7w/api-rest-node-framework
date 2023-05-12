d3717ec2c9e70267b27e28bbcd67209cb1ca582e
Rafael William <rafael.fim@hotmail.com>
2023-05-12 14:04:26 -0300

    testing /transaction post

--- src/test/example.spec.ts
+++ src/test/example.spec.ts
@@ -1,8 +1,34 @@
-import { expect, test } from 'vitest'
+import { test, beforeAll, afterAll } from 'vitest'
+import request from 'supertest' 
+import { app } from '../app'
 
+// executar um código antes que todos os testes executem uma única vez
+// no vitest temos também o beforeEah, afterAll e afterEach
+beforeAll(async () => {
+  await app.ready()
+})
 
-test('o usuário consegue criar uma nova transação', () => {
-  const responseStatusCode = 201
+afterAll(async () => {
+  // remover a aplicação da memória
+  await app.close()
+})
 
-  expect(responseStatusCode).toEqual(201)
+test('user can create a new transaction', async () => {
+  /* const response = await request(app.server)
+    .post('transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit'
+    })
+
+  expect(response.statusCode).toEqual(201) */
+
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit'
+    }).expect(201)
 })
